67c496743c4f2284c06563af5b782f2185d6c3da
Robert Wohlfarth <rbwohlfarth@gmail.com>
2017-04-12 00:02:49 +0000

    Restarting work after a brief hiatus. Saving the current state so that I can always go back.

--- index.js
+++ index.js
@@ -176,48 +176,15 @@ app.post('/pi/scan', function (req, res) {
     }
 });
 app.get('/pi/scan/image', function (req, res) {
-    temp.open({
-        dir: scans,
-        prefix: 'pi-print-scan-image',
-        suffix: 'pnm'
-    }, function (err, info) {
+    exec('./scan-image.sh', function (err, stdout, stderr) {
         if (!err) {
-            fs.close(info.fd, null);
-            exec('scanimage --resolution 600 >'+info.path, function (err, stdout, stderr) {
-                if (!err) {
-                    var jpeg = info.path.replace(/\.pnm$/i, '.jpg');
-                    exec('gm convert '+jpeg, function (err, stdout, stderr) {
-                        if (!err) {
-                            var s = fs.createReadStream(jpeg);
-                            s.on('open', function () {
-                                res.set('Content-Type', 'image/jpeg');
-                                s.pipe(res);
-                            });
-                            s.on('error', function () {
-                                res.set('Content-Type', 'text/plain');
-                                res.status(404).end('Not found');
-                            });
-                            s.on('close', function () {
-                                fs.unlink(jpeg, null);
-                                fs.unlink(info.path, null);
-                            });
-                        } else {
-                            fs.unlink(info.path, null);
-                            res.render('index', {
-                                error: 'No space left for scanning.',
-                                pages: req.body.pages,
-                                total: req.body.total
-                            });
-                        }
-                    });
-                } else {
-                    fs.unlink(info.path, null);
-                    res.render('index', {
-                        error: 'No space left for scanning.',
-                        pages: req.body.pages,
-                        total: req.body.total
-                    });
-                }
+            res.set('Content-Type', 'image/jpeg');
+            res.end(new Buffer(stdout, 'binary'));
+        }
+        else {
+            res.render('index', {
+                error: 'No space left to scan the picture.',
+                ip: os.networkInterfaces().eth0[0].address
             });
         }
     });
@@ -225,7 +192,12 @@ app.get('/pi/scan/image', function (req, res) {
 app.get('/pi/scan/reset', function (req, res) {
     exec('systemd restart saned', function (err, stdout, stderr) {
         if (!err) { res.redirect('/'); }
-        else { res.render('index', {error: 'Scanner reset FAILED. Please try again.'}) }
+        else { 
+            res.render('index', {
+                error: 'Scanner reset FAILED. Please try again.',
+                ip: os.networkInterfaces().eth0[0].address
+            });
+        }
     });
 });
 app.use(express.static('static'));
